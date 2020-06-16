7e7fd4b4c3775f95737076f53f993122074fafa3
Aditya Nair <nair.aditya.r@gmail.com>
2020-06-16 13:27:07 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+
 scene.setBackgroundImage(img`
     9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
     9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
@@ -53,7 +54,7 @@ scene.setBackgroundImage(img`
     d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d 9 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d
     d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 7 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d
     d d d d d d d 9 9 9 9 9 7 7 7 7 7 7 7 7 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d
-    d d d d d d d 9 9 7 7 7 7 7 7 7 7 7 7 7 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d 9 9 9 9 d d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d 3 d d
+    d d d d d d d 9 9 7 7 7 7 7 7 7 7 7 7 7 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d 9 9 9 9 d d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d
     d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d 9 9 d d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d
     d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d
     d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d d d 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d d d d d d d d d d d d d d d d d d d d d d d d d d d 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 d d d d d d
@@ -122,35 +123,30 @@ scene.setBackgroundImage(img`
 `)
 
 let player = sprites.create(img`
-    . . . . . . . . . . . . . . . . . . . . . . . .
-    . . . . . . f f f f . . . . . . . . . . . . . .
-    . . . . f f f 2 2 f f f . . . . . . . . . . . .
-    . . . f f f 2 2 2 2 f f f . . . . . . . . . . .
-    . . f f f e e e e e e f f f . . . . . . . . . .
-    . . f f e 2 2 2 2 2 2 e e f . . . . . . . . . .
-    . . f e 2 f f f f f f 2 e f . . . . . . . . . .
-    . . f f f f e e e e f f f f . . . . . . . . . .
-    . f f e f b f 4 4 f b f e f f . . . . . . . . .
-    . f e e 4 1 f d d f 1 4 e e f . . . . . . . . .
-    f d f e e d d d d d 4 e f f . . . . . . . . . .
-    f b f f e e 4 4 4 e d d 4 e . . . . . . . . . .
-    f b f 4 f 2 2 2 2 e d d e . . . . . . . . . . .
-    f c f . f 2 2 c c c e e . . . . . . . . . . . .
-    . f f . f 4 4 c d c 4 f . . . . . . . . . . . .
-    . . . . f f f d d c f f . . . . . . . . . . . .
-    . . . . . f d d c f f . . . . . . . . . . . . .
-    . . . . c d d c . . . . . . . . . . . . . . . .
-    . . . . c d c . . . . . . . . . . . . . . . . .
-    . . . . c c . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . . . . . . . . . .
-`)
-` , SpriteKind.Player)
+    . . . . . . f f f f . . . . . .
+    . . . . f f f 2 2 f f f . . . .
+    . . . f f f 2 2 2 2 f f f . . .
+    . . f f f e e e e e e f f f . .
+    . . f f e 2 2 2 2 2 2 e e f . .
+    . . f e 2 f f f f f f 2 e f . .
+    . . f f f f e e e e f f f f . .
+    . f f e f b f 4 4 f b f e f f .
+    . f e e 4 1 f d d f 1 4 e e f .
+    . . f e e d d d d d d e e f . .
+    . . . f e e 4 4 4 4 e e f . . .
+    . . e 4 f 2 2 2 2 2 2 f 4 e . .
+    . . 4 d f 2 2 2 2 2 2 f d 4 . .
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+    . . . . . f f f f f f . . . . .
+    . . . . . f f . . f f . . . . .
+`, SpriteKind.Player)
+
 controller.moveSprite(player)
 
-let duck = sprites.create(img``)
+info.setScore(0)
+info.startCountdown(10)
+
+let duck = sprites.create(img`
     . . . . . . . . . . . . . . . .
     . . . . . . . . . . b 5 b . . .
     . . . . . . . . . b 5 b . . . .
@@ -166,10 +162,16 @@ let duck = sprites.create(img``)
     c b d d d d d 5 5 5 5 5 5 5 b .
     . c d d d d d d 5 5 5 5 5 d b .
     . . c b d d d d d 5 5 5 b b . .
-    . . . c c c c c c c c b b . . .;
+    . . . c c c c c c c c b b . . .
+`, SpriteKind.Enemy)
 
+duck.setPosition(Math.randomRange(0, scene.screenWidth()), Math.randomRange(0, scene.screenHeight()))
 
-duck,setPosition(Math.randomRange(0, scene.screenWidth()),Math.randomRange(0, scene.screenHeight()))
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function () {
+    duck.setPosition(Math.randomRange(0, scene.screenWidth()), Math.randomRange(0, scene.screenHeight()))
+    info.changeScoreBy(1)
+})
 
-// sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, othersprite) {
-// duck,setPosition(Math.randomRange(0, scene.screenWidth()),Math.randomRange(0, scene.screenHeight()))}
+game.onUpdate(function () {
+
+})
